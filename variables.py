e1235c0f33ff3994fd6d78b9e5cc0239b9bb5a16
bhavyamoosara <bhavyamoosara@gmail.com>
2026-02-14 10:55:37 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,20 @@
+# Task 1
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+# Task 2: 
+#for = 4
+print("\nYou cannot use 'for' as a variable name because it is a reserved keyword in Python.")
+
+# Task 3
+principal = 1000  
+rate = 5          
+time = 3          
+
+simple_interest = (principal * rate * time) / 100
+
+print("\nPrincipal:", principal)
+print("Rate of Interest:", rate)
+print("Time:", time)
+print("Simple Interest:", simple_interest)
